1e1b8b678be1ac1e4073311b24c57540a4f6cc30
George Moraitis <moraitis.georgios@icloud.com>
2022-07-11 22:49:11 +0300

    Update function to use map for iteration

--- src/components/MatchesPresentation.js
+++ src/components/MatchesPresentation.js
@@ -17,36 +17,15 @@ function MatchesPresentation(props) {
                 </thead>
                 <tbody>
 
-                    <tr>
-                        <td>{props.data[0].date}</td>
-                        <td>{props.data[0].homeTeam} - {props.data[0].awayTeam}</td>
-                        <td>{props.data[0].homeScore} - {props.data[0].awayScore}</td>
-                        <td colSpan={1}>{props.data[0].events}</td>
-                    </tr>
-                    <tr>
-                        <td>{props.data[1].date}</td>
-                        <td>{props.data[1].homeTeam} - {props.data[1].awayTeam}</td>
-                        <td>{props.data[1].homeScore} - {props.data[1].awayScore}</td>
-                        <td colSpan={1}>{props.data[1].events}</td>
-                    </tr>
-                    <tr>
-                        <td>{props.data[2].date}</td>
-                        <td>{props.data[2].homeTeam} - {props.data[2].awayTeam}</td>
-                        <td>{props.data[2].homeScore} - {props.data[2].awayScore}</td>
-                        <td colSpan={1}>{props.data[2].events}</td>
-                    </tr>
-                    <tr>
-                        <td>{props.data[3].date}</td>
-                        <td>{props.data[3].homeTeam} - {props.data[3].awayTeam}</td>
-                        <td>{props.data[3].homeScore} - {props.data[3].awayScore}</td>
-                        <td colSpan={1}>{props.data[3].events}</td>
-                    </tr>
-                    <tr>
-                        <td>{props.data[4].date}</td>
-                        <td>{props.data[4].homeTeam} - {props.data[4].awayTeam}</td>
-                        <td>{props.data[4].homeScore} - {props.data[4].awayScore}</td>
-                        <td colSpan={1}>{props.data[4].events}</td>
-                    </tr>
+                        {props.data.map(match => (
+                            <tr key={match.id}>
+                                <td>{match.date}</td>
+                                <td>{match.homeTeam} - {match.awayTeam} </td>
+                                <td>{match.homeScore} - {match.awayScore}   </td>
+                                <td>{match.events}</td>
+                            </tr>))}
+                
+
                 </tbody>
 
             </Table>
